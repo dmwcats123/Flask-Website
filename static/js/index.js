adfadc8d0dbf0f0e49350ab08864215d92b6d82a
dmwcats <dmwcats123@gmail.com>
2023-04-03 22:02:05 -0500

    huge commit, forgot I implemented version control

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,15 @@ var mywindow = $(window);
 var mypos = mywindow.scrollTop();
 var up = false;
 var newscroll;
+const home_text = document.querySelector("#home_text");
+const str_home_text = home_text.textContent;
+const str_home_text_split = str_home_text.split("");
+
+
+for (let i = 0; i < str_home_text_split; i++) {
+    home_text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
 mywindow.scroll(function () {
     newscroll = mywindow.scrollTop();
     if (newscroll > mypos && !up) {
@@ -10,6 +19,7 @@ mywindow.scroll(function () {
         console.log(up);
     } else if(newscroll < mypos && up) {
         $('#navbar').stop().fadeIn();
+        
         up = !up;
     }
     mypos = newscroll;
