c8e6b7330b0bead5d65a6d76e73b6c0e8a1f2218
dmwcats <dmwcats123@gmail.com>
2023-04-04 11:42:10 -0500

    ios navabar top of page bug fix take 2

--- static/js/index.js
+++ static/js/index.js
@@ -6,17 +6,15 @@ var newscroll;
 
 mywindow.scroll(function () {
     newscroll = mywindow.scrollTop();
-    if (newscroll > 0) {
+    if (newscroll > 2) {    
         if (newscroll > mypos && !up) {
-            $('#navbar').stop().fadeOut();
-            up = !up;
-            console.log(up);
-        } else if(newscroll < mypos && up) {
-            $('#navbar').stop().fadeIn();
-            
-            up = !up;
-        }
-        mypos = newscroll;
+        $('#navbar').stop().fadeOut();
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('#navbar').stop().fadeIn();
+        up = !up;
     }
+    mypos = newscroll;}
 
 });
