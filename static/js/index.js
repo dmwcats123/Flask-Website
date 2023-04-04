89f05e153ed231e07b2563a869160e2684728ae9
dmwcats <dmwcats123@gmail.com>
2023-04-04 11:38:42 -0500

    deleted extra, fixed ios navabar scroll bug

--- static/js/index.js
+++ static/js/index.js
@@ -2,25 +2,21 @@ var mywindow = $(window);
 var mypos = mywindow.scrollTop();
 var up = false;
 var newscroll;
-const home_text = document.querySelector("#home_text");
-const str_home_text = home_text.textContent;
-const str_home_text_split = str_home_text.split("");
 
-
-for (let i = 0; i < str_home_text_split; i++) {
-    home_text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
 
 mywindow.scroll(function () {
     newscroll = mywindow.scrollTop();
-    if (newscroll > mypos && !up) {
-        $('#navbar').stop().fadeOut();
-        up = !up;
-        console.log(up);
-    } else if(newscroll < mypos && up) {
-        $('#navbar').stop().fadeIn();
-        
-        up = !up;
+    if (newscroll > 0) {
+        if (newscroll > mypos && !up) {
+            $('#navbar').stop().fadeOut();
+            up = !up;
+            console.log(up);
+        } else if(newscroll < mypos && up) {
+            $('#navbar').stop().fadeIn();
+            
+            up = !up;
+        }
+        mypos = newscroll;
     }
-    mypos = newscroll;
+
 });
